bd874d5aedee22f080c84e598d5d5fd6a530c5a7
Claes Nymand Nilsson <claes89@gmail.com>
2024-05-02 22:54:34 +0200

    Add more projects

--- app/software/page.tsx
+++ app/software/page.tsx
@@ -19,21 +19,35 @@ const projects: SoftwareProject[] = [
     description: "This website :) Also, the Next.js version of claesnn.com",
     url: "https://www.claesnn.com",
     github: "https://github.com/claesnn/next-claesnn",
-    tags: ["SSG", "Next.js", "Router"],
+    tags: ["SSG", "Next.js", "React"],
   },
   {
     name: "Claesnn.com - SvelteKit",
     description: "The SvelteKit version of claesnn.com",
     url: "https://svelte-claesnn.pages.dev/",
     github: "https://github.com/claesnn/svelte-claesnn",
-    tags: ["SSG", "SvelteKit", "Router"],
+    tags: ["SSG", "Svelte", "SvelteKit"],
   },
   {
     name: "Claesnn.com - TanStack Router",
     description: "The TanStack Router (client side) version of claesnn.com",
     url: "https://tan-claesnn.pages.dev/",
     github: "https://github.com/claesnn/tan_claesnn",
-    tags: ["CSR", "TanStack", "Client", "Router"],
+    tags: ["CSR", "TanStack", "React"],
+  },
+  {
+    name: "Claesnn.com - Nuxt.js",
+    description: "The Nuxt.js (vue) version of claesnn.com",
+    url: "https://nuxt-claesnn.pages.dev/",
+    github: "https://github.com/claesnn/nuxt-claesnn",
+    tags: ["SSG", "Nuxt", "Vue"],
+  },
+  {
+    name: "Claesnn.com - Vue.js",
+    description: "The Vue.js version of claesnn.com",
+    url: "https://website-ch7.pages.dev/",
+    github: "https://github.com/claesnn/website",
+    tags: ["CSR", "Vue"],
   },
 ];
 
