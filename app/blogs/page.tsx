77d43996f567f008181702e4d0ca91e650add2f0
Claes Nymand Nilsson <claes89@gmail.com>
2024-05-03 19:19:49 +0200

    Add Suspense boundary

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,12 +1,15 @@
 import { getBlogPosts } from "@/lib/blogs";
 import BlogList from "./BlogList";
+import { Suspense } from "react";
 
 export default function Page() {
   const blogs = getBlogPosts();
 
   return (
     <div className="lg:flex justify-between">
-      <BlogList blogs={blogs} />
+      <Suspense>
+        <BlogList blogs={blogs} />
+      </Suspense>
     </div>
   );
 }
