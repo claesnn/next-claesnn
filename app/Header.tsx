f426e4197b97c1a9b6616c4d702f011db5c5d079
Claes Nymand Nilsson <claes89@gmail.com>
2024-05-06 21:18:35 +0200

    Fix add useEffect

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { oswald } from "@/lib/fonts";
 import { usePathname } from "next/navigation";
 
