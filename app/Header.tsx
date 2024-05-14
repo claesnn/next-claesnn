82bfcc2828f61c49a44c52ff0d0b86eaab29f9ab
Claes Nymand Nilsson <claes89@gmail.com>
2024-05-14 18:20:18 +0200

    Add logo back

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,11 +1,11 @@
-"use client";
+"use client"
 
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import { oswald } from "@/lib/fonts";
-import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { oswald } from "@/lib/fonts"
+import { usePathname } from "next/navigation"
 
 const links = [
   {
@@ -20,86 +20,113 @@ const links = [
     name: "Photography",
     route: "/photography",
   },
-];
+]
 
 function SvgMenu() {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="lucide lucide-menu"
-    >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className='lucide lucide-menu'>
+      <line
+        x1='4'
+        x2='20'
+        y1='12'
+        y2='12'
+      />
+      <line
+        x1='4'
+        x2='20'
+        y1='6'
+        y2='6'
+      />
+      <line
+        x1='4'
+        x2='20'
+        y1='18'
+        y2='18'
+      />
     </svg>
-  );
+  )
 }
 
 function SvgClose() {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="lucide lucide-x"
-    >
-      <path d="M18 6 6 18" />
-      <path d="m6 6 12 12" />
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className='lucide lucide-x'>
+      <path d='M18 6 6 18' />
+      <path d='m6 6 12 12' />
     </svg>
-  );
+  )
 }
 
 export default function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const pathname = usePathname();
+  const [menuOpen, setMenuOpen] = useState(false)
+  const pathname = usePathname()
 
   function closeMenu() {
-    setMenuOpen(false);
-    document.body.style.overflow = "visible";
+    setMenuOpen(false)
+    document.body.style.overflow = "visible"
   }
 
   function toggleMenu() {
-    document.body.style.overflow = menuOpen ? "visible" : "hidden";
-    setMenuOpen(!menuOpen);
+    document.body.style.overflow = menuOpen ? "visible" : "hidden"
+    setMenuOpen(!menuOpen)
   }
 
   function checkCloseMenu(url: string) {
     if (menuOpen && url === pathname) {
-      closeMenu();
+      closeMenu()
     }
   }
 
   useEffect(() => {
-    closeMenu();
-  }, [pathname]);
+    closeMenu()
+  }, [pathname])
 
   return (
     <>
-      <header className="shadow-sm w-full fixed top-0 bg-white opacity-95">
-        <div className="flex justify-between px-4 py-3 place-items-center max-w-7xl mx-auto ">
-          <Link href="/" onClick={() => checkCloseMenu("/")}>
-            <h1 className={cn("text-2xl", oswald.className)}>CLAESNN</h1>
+      <header className='shadow-sm w-full fixed top-0 bg-white opacity-95'>
+        <div className='flex justify-between px-4 py-3 place-items-center max-w-7xl mx-auto '>
+          <Link
+            href='/'
+            onClick={() => checkCloseMenu("/")}
+            className='flex place-items-center'>
+            <img
+              src='/logo.jpg'
+              alt='logo'
+              className='w-8 h-8 mr-3'
+            />
+            <h1
+              className={cn("text-2xl", oswald.className)}
+              onClick={closeMenu}>
+              CLAESNN
+            </h1>
           </Link>
 
-          <button onClick={toggleMenu} className="sm:hidden">
+          <button
+            onClick={toggleMenu}
+            className='sm:hidden'>
             {!menuOpen ? <SvgMenu /> : <SvgClose />}
           </button>
 
-          <nav className="hidden gap-6 sm:flex">
+          <nav className='hidden gap-6 sm:flex'>
             {links.map((link) => (
               <Link
                 href={link.route}
@@ -108,8 +135,7 @@ export default function Header() {
                     ? "underline text-blue-900"
                     : ""
                 }`}
-                key={link.name}
-              >
+                key={link.name}>
                 {link.name}
               </Link>
             ))}
@@ -117,21 +143,21 @@ export default function Header() {
         </div>
       </header>
       {menuOpen && (
-        <nav className="w-full z-10 fixed top-[50px] flex">
+        <nav className='w-full z-10 fixed top-[50px] flex'>
           <div
             onClick={closeMenu}
-            className=" bg-gray-900 h-screen flex-grow opacity-40"
-          ></div>
-          <aside className="w-[20rem] bg-white h-screen p-3 flex flex-col">
+            className=' bg-gray-900 h-screen flex-grow opacity-40'></div>
+          <aside className='w-[20rem] bg-white h-screen p-3 flex flex-col'>
             {links.map((link) => (
-              <Link href={link.route} key={link.name}>
+              <Link
+                href={link.route}
+                key={link.name}>
                 <Button
-                  variant="link"
+                  variant='link'
                   className={cn("text-foreground text-lg", {
                     underline: pathname.startsWith(link.route),
                   })}
-                  onClick={() => checkCloseMenu(link.route)}
-                >
+                  onClick={() => checkCloseMenu(link.route)}>
                   {link.name}
                 </Button>
               </Link>
@@ -140,5 +166,5 @@ export default function Header() {
         </nav>
       )}
     </>
-  );
+  )
 }
