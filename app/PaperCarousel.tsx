0c1299d2ace3a60a9481d281eb7ab512200c8a89
Claes Nymand Nilsson <claes89@gmail.com>
2024-10-16 19:06:59 +0200

    Add back-forward buttons

--- app/PaperCarousel.tsx
+++ app/PaperCarousel.tsx
@@ -3,6 +3,7 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
@@ -56,10 +57,29 @@ export default function PaperCarousel() {
       url: "https://pubmed.ncbi.nlm.nih.gov/26657798/",
       image: "/papers/0.webp",
     },
+
+    {
+      title:
+        "Semi-Versatile microscale screening platform for improving recombinant protein production in Chinese hamster ovary cells",
+      description:
+        "In this project, we developed a screening platform in 96-deep well plates. The platform enabled semi-automated cell counting, high-throughput protein secretion evaluation using split-GFP and the plate format enabled fast transfection and screening.<br /><br />My contribution to the project included cloning of plasmids encoding genes related to protein production and secretion. Evaluating the system for testing improvement of protein production and secretion for difficult-to-express proteins.",
+      tags: [
+        "Split-GFP",
+        "Celigo",
+        "Deep Well Plates",
+        "Duetz",
+        "Transient transfection",
+        "CHO",
+        "Protein Secretion",
+      ],
+      url: "https://pubmed.ncbi.nlm.nih.gov/26657798/",
+      image: "/papers/0.webp",
+    },
   ];
 
   const paper = papers.length >= paperInt ? papers[paperInt] : papers[0];
 
+  const prevPaper = (paperInt - 1 + papers.length) % papers.length;
   const nextPaper = (paperInt + 1) % papers.length;
   const setPaper = (paper: number) => {
     const params = new URLSearchParams(searchParams);
@@ -99,13 +119,15 @@ export default function PaperCarousel() {
             </CardContent>
           </Card>
         </Link>
-        <div className="mx-auto mt-4">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => setPaper(nextPaper)}
-          >
-            Next paper
+        <div className="mx-auto mt-4 place-items-center flex">
+          <Button variant="ghost" size="sm" onClick={() => setPaper(prevPaper)}>
+            <ArrowLeft size={18} />
+          </Button>
+          <span className="mx-4 text-xs text-slate-500">
+            {paperInt + 1}/{papers.length}
+          </span>
+          <Button variant="ghost" size="sm" onClick={() => setPaper(nextPaper)}>
+            <ArrowRight size={18} />
           </Button>
         </div>
       </div>
